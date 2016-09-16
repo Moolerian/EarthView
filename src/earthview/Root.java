7f81350be8487e6a055c62bb9db73dd44dc70185
Moolerian <Moolerian@Gmail.com>
2016-09-16 14:30:38 +0430

    PUSH-16-09 #2

--- src/earthview/Root.java
+++ src/earthview/Root.java
@@ -9,12 +9,12 @@ package earthview;
  *
  * @author Mohammad
  */
-public class root extends javax.swing.JFrame {
+public class Root extends javax.swing.JFrame {
 
     /**
      * Creates new form root
      */
-    public root() {
+    public Root() {
         initComponents();
     }
 
@@ -124,20 +124,21 @@ public class root extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(root.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Root.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(root.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Root.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(root.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Root.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(root.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Root.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new root().setVisible(true);
+                new Root().setVisible(true);
             }
         });
     }
